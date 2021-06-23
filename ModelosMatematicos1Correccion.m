52356b345e2351c452fa3b7f4d73d77e2836025b
erileonardo <82676091+erileonardo@users.noreply.github.com>
2021-06-23 01:57:08 -0500

    Add files via upload

---
+++ ModelosMatematicos1Correccion.m
@@ -0,0 +1,23 @@
+% Octave Script
+% Title       : Modelos Matematicos
+% Description : Graficar  
+% Author      : Erika Amairani Leonardo Arce
+% Date        : 23 de junio 2021
+% Version     : 1
+% Notes       : funcion f(x)=2-?4x-4
+
+
+clc
+
+%Grafica
+x=1:0.1:5;
+y=(2-(sqrt(4*x)))-2;
+% Dibujar grafica
+plot(x,y)
+grid on; 
+% Titulo
+title("f(x)=2-?4x-4");
+% Etiqueta para x
+xlabel("X");
+% Etiqueta para y
+ylabel("Y");
